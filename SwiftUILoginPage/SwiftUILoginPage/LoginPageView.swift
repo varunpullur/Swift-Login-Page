dcb0ada38d01e0cee71016068358964bffcd54d8
varun pullur <varun.pullur95@gmail.com>
2023-09-06 18:23:20 +0530

    Login page using SwiftUI completed

--- SwiftUILoginPage/SwiftUILoginPage/LoginPageView.swift
+++ SwiftUILoginPage/SwiftUILoginPage/LoginPageView.swift
@@ -8,12 +8,58 @@
 import SwiftUI
 
 struct LoginPageView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var message: String = ""
+    
+    var isiPad: Bool {
+        return UIDevice.current.userInterfaceIdiom == .pad
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Sign Up")
+                .font(.largeTitle)
+                .padding(.top, 40)
+                .padding(.bottom, 80)
+            
+            TextField("Username", text: $username)
+                .modifier(LoginTextFieldStyle())
+                .frame(width: isiPad ? 400 : nil)
+            
+            SecureField("Password", text: $password)
+                .modifier(LoginTextFieldStyle())
+                .frame(width: isiPad ? 400 : nil)
+            
+            Button{
+                if username == "admin" && password == "admin" {
+                    message = "Login Successful"
+                } else if username.isEmpty || password.isEmpty {
+                    message = "Please enter all the fields"
+                } else {
+                    message = "Invalid username or password"
+                }
+                                
+            } label: {
+                Text("Login")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 220, height: 60)
+                    .background(Color.blue)
+                    .cornerRadius(15.0)
+            }
+               
+            Text(message)
+                .foregroundColor(message == "Login Successful" ? .green : .red)
+                .padding()
+            
+            Button{} label: {
+                Text("Forgot Password?")
+                    .font(.subheadline)
+                    .foregroundColor(.blue)
+            }
+            Spacer()
         }
         .padding()
     }
@@ -22,5 +68,17 @@ struct LoginPageView: View {
 struct LoginPageView_Previews: PreviewProvider {
     static var previews: some View {
         LoginPageView()
+    }
+}
+
+struct LoginTextFieldStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .autocapitalization(.none)
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+            .padding(.horizontal, 20)
     }
 }
