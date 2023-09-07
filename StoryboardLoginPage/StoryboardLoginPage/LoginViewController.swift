b9d17cc8979fa523f36f75fc58fe9c8f503d38cc
varun pullur <varun.pullur95@gmail.com>
2023-09-07 14:56:25 +0530

    Login page using Storyboard completed

--- StoryboardLoginPage/StoryboardLoginPage/LoginViewController.swift
+++ StoryboardLoginPage/StoryboardLoginPage/LoginViewController.swift
@@ -9,8 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var btnLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        
+        usernameField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        passwordField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    private func setupUI() {
+        btnLogin.isEnabled = false
+    }
+    
+    @IBAction func loginBtnClicked(_ sender: UIButton) {
+        guard let username = usernameField.text,
+              let password = passwordField.text else {
+            return
+        }
+        
+        print("username: \(username) \npassword: \(password)")
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if let username = usernameField.text,
+           let password = passwordField.text,
+           !username.isEmpty,
+           !password.isEmpty {
+            btnLogin.isEnabled = true
+        } else {
+            btnLogin.isEnabled = false
+        }
     }
 }
